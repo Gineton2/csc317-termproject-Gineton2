f22cd26a33e8d2c805b78353ea04c0a07360a454
Gineton Alencar II <gineton2@gmail.com>
2021-04-06 23:59:42 -0700

    Upload changes

--- js/photos.js
+++ js/photos.js
@@ -1,11 +1,17 @@
 // from Sample Solution video:
 function fadeOut(event){}
 
-function createPhotoCard(data, containerDiv){}
+function createPhotoCard(data, containerDiv){
+    let div = document.createElement('image-post');
+    let img = document.createElement('img');
+    img.src = document.url;
+    div.appendChild(img);
+    return div;
+}
 
 let mainDiv = document.getElementById("container");
 if(mainDiv){
-    let fetchURL = ""
+    let fetchURL = "https://jsonplaceholder.typicode.com/albums/2/photos"
     fetch(fetchURL)
     .then((data) => data.json())
     .then((photos) => {
@@ -15,4 +21,7 @@ if(mainDiv){
         });
         document.getElementById('items-count').innerHTML="There are $(photos.length) photo(s).";
     })
+    .catch((error =>{
+        console.log(error);
+    }));
 }
