cbb9a4b4926dbe2acbf12ea741a2c7bc2f848e0e
Gineton Alencar II <Gineton2@users.noreply.github.com>
2023-05-17 23:19:44 -0700

    Complete fetching posts part of A4

--- js/photos.js
+++ js/photos.js
@@ -1,16 +1,15 @@
 // from Sample Solution video:
 function fadeOut(event) {
-    let targetElement = event.target;
-    target.style.opacity = 0;
-    setTimeout(() => {
-        targetElement.remove();
-        }, 500);
+  let targetElement = event.target;
+  target.style.opacity = 0;
+  setTimeout(() => {
+    targetElement.remove();
+  }, 500);
 }
 
 function createPhotoCard(data, containerDiv) {
   let div = document.createElement("div");
-//   div.classList.add("container");
-  div.classList.add("image-post-container")
+  div.classList.add("image-post-container");
   containerDiv.appendChild(div);
 
   let title = document.createElement("h1");
@@ -18,29 +17,25 @@ function createPhotoCard(data, containerDiv) {
   title.textContent = data.title;
   div.appendChild(title);
 
-
   let img = document.createElement("img");
   img.classList.add("posted-image");
   img.src = data.url;
-  img.onclick = fadeOut;
   div.appendChild(img);
 
   let belowImage = document.createElement("div");
-    belowImage.classList.add("below-image");
-    div.appendChild(belowImage);
+  belowImage.classList.add("below-image");
+  div.appendChild(belowImage);
 
-    let albumId = document.createElement("p");
-    // add albumId and id in place of post-author and post-date
-    albumId.classList.add("post-author");
-    albumId.textContent = "Album ID: " + data.albumId;
-    belowImage.appendChild(albumId);
+  let albumId = document.createElement("p");
+  // add albumId and id in place of post-author and post-date
+  albumId.classList.add("post-info");
+  albumId.textContent = "Album: " + data.albumId;
+  belowImage.appendChild(albumId);
 
-    let photoId = document.createElement("p");
-    photoId.classList.add("post-date");
-    photoId.textContent = "Photo ID: " + data.id;
-    belowImage.appendChild(photoId);
-
-
+  let photoId = document.createElement("p");
+  photoId.classList.add("post-info");
+  photoId.textContent = "Photo: " + data.id;
+  belowImage.appendChild(photoId);
   return div;
 }
 
@@ -50,12 +45,15 @@ if (mainDiv) {
   fetch(fetchURL)
     .then((data) => data.json())
     .then((photos) => {
-      //   let innerHTML = "";
       photos.forEach((photo) => {
         createPhotoCard(photo, mainDiv);
       });
+      if (photos.length == 1) {
+        document.getElementById("items-count").innerHTML = "There is one photo.";
+      } else { 
       document.getElementById("items-count").innerHTML =
-        ("There are " + photos.length + " photo(s).");
+        "There are " + photos.length + " photos.";
+      }
     })
     .catch((error) => {
       console.log(error);
