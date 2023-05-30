dba4b14af8adaaebbb4757657ce452db0c2efeb6
Gineton Alencar II <Gineton2@users.noreply.github.com>
2023-05-29 21:17:08 -0400

    Use async/await for mysql2 and promises permission

--- application/routes/sampadb.js
+++ application/routes/sampadb.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 
-router.get('/getAllUsers', (req, res, next) => {
-    db.query('SELECT * from users;', (err, results, fields) => {
+// router.get('/getAllUsers', (req, res, next) => {
+//     db.query('SELECT * from users;', (err, results, fields) => {
+//         console.log(results);
+//         res.send(results);
+//     });
+// });
+// use async/await to due to incompatibility with mysql2 and promises
+router.get('/getAllUsers', async (req, res, next) => {
+    try {
+        const [results, fields] = await db.query('SELECT * from users;');
         console.log(results);
         res.send(results);
-    });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error occurred during query execution");
+    }
 });
 
 module.exports = router;
