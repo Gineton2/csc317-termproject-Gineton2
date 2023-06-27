90bc72e2b0e52fd618a5e8c140b003f61f79e4de
Gineton Alencar II <Gineton2@users.noreply.github.com>
2023-06-27 16:57:27 -0700

    Write MySQL scripts for db creation and updating

---
+++ sql/sampadb_insert_posts.sql
@@ -0,0 +1,14 @@
+INSERT INTO `sampadb`.`posts`
+(`title`,
+`description`,
+`photopath`,
+`thumbnail`,
+`created`,
+`fk_userid`)
+VALUES
+("",
+"",
+"",
+"",
+"",
+"");
---
+++ sql/sampadb_update_username.sql
@@ -0,0 +1,5 @@
+SELECT * from sampadb.users;
+UPDATE `sampadb`.`users`
+SET
+`username` = "dangerousscript"
+WHERE `id` = 1;
