b46e3ccbc46a3bfd6f10c3d1b6ef9546da4c0802
Gineton Alencar II <Gineton2@users.noreply.github.com>
2023-09-13 19:33:31 -0700

    Implement search front-end

--- application/public/js/front-end.js
+++ application/public/js/front-end.js
@@ -1,3 +1,64 @@
+// const { execute } = require("../../config/database");
+
+function executeSearch() {
+    let searchTerm = document.getElementById("search-input").value;
+    if (!searchTerm) {
+        location.replace('/');
+        return;
+    }
+    const imagePosts = document.getElementById("image-posts");
+    let searchURL = `/posts/search?search=${searchTerm}`;
+    fetch(searchURL)
+        .then((data) => {
+            return data.json();
+        })
+        .then((data_json) => {
+            let newImagePostsHTML = "";
+            data_json.results.forEach((row) => {
+                newImagePostsHTML += createCard(row);
+            });
+            imagePosts.innerHTML = newImagePostsHTML;
+            if (data_json.message){
+                addFlashFromFrontEnd(data_json.message);
+            }
+        })
+}
+
+function addFlashFromFrontEnd(message) {
+    let flashMessageDiv = document.createElement("div");
+    let alertDiv = document.createElement("div");
+    let innerTextNode = document.createTextNode(message);
+    alertDiv.appendChild(innerTextNode);
+    flashMessageDiv.appendChild(alertDiv);
+    flashMessageDiv.id = "flash-message";
+    alertDiv.id = "alert-good";
+    alertDiv.className = "alert";
+    const headerElement = document.getElementsByTagName('header')[0];
+    if (headerElement) {
+        headerElement.insertAdjacentElement('afterend', flashMessageDiv);
+    } else {
+        document.body.insertBefore(flashMessageDiv, document.body.firstChild);
+    }
+    setFlashMessageFadeOut();
+}
+
+function createCard(postData) {
+    return `<div id="post-${postData.id}" class="image-post-container">
+            <h1 class="title">${postData.title}</h1>
+            <div class="image-container">
+            <img class="posted-image" alt="Missing" src="${postData.thumbnail}" />
+            </div>
+            <div class="below-image">
+                <div class="post-authorship-info">
+                    <p class="post-date">${postData.created_formatted}</p>
+                    <p class="post-author">${postData.username}</p>
+                </div>
+                <p class="post-description">${postData.description}</p>
+                <a href="/post-details/${postData.id}" class="post-details-button">View Post</a>
+            </div>
+        </div>`
+}
+
 function setFlashMessageFadeOut() {
     let flashMessageElement = document.getElementById("flash-message");
     if (flashMessageElement) {
@@ -21,4 +82,14 @@ if (document.readyState === 'loading') {
     setFlashMessageFadeOut();
 }
 
-
+let searchButton = document.getElementById("search-button");
+let searchInput = document.getElementById("search-input");
+if (searchInput) {
+    searchButton.onclick = executeSearch;
+    searchInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            executeSearch();
+        }
+    });
+}
