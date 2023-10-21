32ecdb0467aceb49f11977d87ba57a577c82c48b
Gineton Alencar II <Gineton2@users.noreply.github.com>
2023-10-20 19:25:30 -0700

    Add ending semicolons to baseSQL in PostsModel

--- application/models/Posts.js
+++ application/models/Posts.js
@@ -2,7 +2,7 @@ var db = require('../config/database');
 const PostModel = {};
 
 PostModel.create = (title, description, imageUploaded, destinationOfThumbnail, fk_userId) => {
-    const baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_user_id) VALUES (?, ?, ?, ?, now(), ?)';
+    const baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_user_id) VALUES (?, ?, ?, ?, now(), ?);';
     return db.execute(baseSQL, [title, description, imageUploaded, destinationOfThumbnail, fk_userId])
         .then(([results, fields]) => {
             return Promise.resolve(results && results.affectedRows);
@@ -42,7 +42,7 @@ PostModel.getPostDetailsById = (postId) => {
     let baseSQL = 
     "SELECT p.id, p.title, p.description, p.photopath, DATE_FORMAT(p.created, '%m/%d/%Y') AS created_formatted, \
     u.username FROM posts p INNER JOIN users u \
-    ON p.fk_user_id = u.id WHERE p.id = ?";
+    ON p.fk_user_id = u.id WHERE p.id = ?;";
     return db.execute(baseSQL, [postId])
     .then(([results, fields]) => {
         return Promise.resolve(results);
